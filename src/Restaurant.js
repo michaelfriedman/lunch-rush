6b4e7ec11b18d86b935db4930e1714e59962f629
Michael Friedman <contact@michaeldavidfriedman.com>
2017-04-08 11:11:50 -0700

    Lunch voting app completed.

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -4,12 +4,16 @@ import './Restaurant.css';
 
 class Restaurant extends Component {
   render () {
+    const { name, votes, handleSelect, handleDeselect, user } = this.props;
   const userHasSelected = votes && Object.keys(votes).includes(user.uid);
-  const { name, votes, handleSelect, handleDeselect, user } = this.props;
+
     return (
       <article className="Restaurant">
-        <h3>{ name }</h3>
-        <ul>
+        <h2 className="Restaurant--name">{ name }</h2>
+        <p className="Restaurant--count">
+          Total Votes: {(votes && Object.keys(votes).length) || 0 }
+        </p>
+        <ul className="Restaurant--votes">
           { votes && map(votes, (vote, key) => <li key={key}>{ vote }</li>) }
         </ul>
         {
